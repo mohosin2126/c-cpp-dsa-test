790410b5ef3033763a79dbcbbd4159005ebc2024
mohosin2126 <mohosin2126@gmail.com>
2024-10-26 19:39:13 +0600

    khana pina two solved

---
+++ a-1/khana-pina-two.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int m;
+    scanf("%d", &m);
+    if (m % 3 == 0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
