39362e0685743d6fbb47ab59718457739f5f6cb1
mohosin2126 <mohosin2126@gmail.com>
2024-10-26 19:37:12 +0600

    multiplied solved

---
+++ a-1/multiply.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    long long a, b, multiplication;
+    scanf("%lld %lld", &a, &b);
+    multiplication = a * b;
+    printf("%lld", multiplication);
+
+    return 0;
+}
